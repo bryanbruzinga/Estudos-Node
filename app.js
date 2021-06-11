63cefb578b249d8246c9654f81386acc12f11781
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-06-11 10:00:07 -0300

    seguimento das aulas

--- app.js
+++ app.js
@@ -2,9 +2,27 @@ const express = require("express");
 const handlebars = require("express-handlebars");
 const app = express();
 const admin = require("./routes/admin");
+const path = require("path");
+const mongoose = require("mongoose");
 
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+//Mongoose
+mongoose
+  .connect("mongodb://localhost/blogapp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Conectado ao MongoDB");
+  })
+  .catch((err) => {
+    console.log("Erro ao se conectar ao MongoDB: " + err);
+  });
+
+//Public
+app.use(express.static(path.join(__dirname, "public")));
 
 //Rotas
 app.use("/admin", admin);
