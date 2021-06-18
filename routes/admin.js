9e8a9441b6c5b1925c46234d7ae3195011a0ddbd
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-06-18 14:30:36 -0300

    página postagens

--- routes/admin.js
+++ routes/admin.js
@@ -117,4 +117,20 @@ router.post("/categorias/deletar", (req, res) => {
     });
 });
 
+router.get("/postagens", (req, res) => {
+  res.render("admin/postagens");
+});
+
+router.get("/postagens/add", (req, res) => {
+  Categoria.find()
+    .lean()
+    .then((categorias) => {
+      res.render("admin/addpostagem", { categorias: categorias });
+    })
+    .catch((err) => {
+      req.flash("error_msg", "Houve um erro ao carregar o formulário");
+      res.redirect("/admin");
+    });
+});
+
 module.exports = router;
