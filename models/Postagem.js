3cebea85cc2790ce8abf7a191f7ed21bd9c77114
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-06-20 17:09:40 -0300

    rota postagens

---
+++ models/Postagem.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Postagem = new Schema({
+  titulo: {
+    type: String,
+    required: true,
+  },
+  descricao: {
+    type: String,
+    required: true,
+  },
+  conteudo: {
+    type: String,
+    required: true,
+  },
+  categoria: {
+    type: String,
+    required: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+  },
+});
+
+mongoose.model("postagens", Postagem);
